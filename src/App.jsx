57afe44b09ad68ff7925ff155ab84101e19b6f81
fdimas157 <fdimas157@gmail.com>
2023-06-05 20:41:47 +0700

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Kotak from "./components/Kotak";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const listProduct = [
+    {
+      id: 1,
+      productName: "Sneakers Converse CTAS",
+      productPrice: "Rp. 599.000",
+      urlImage:
+        "https://www.converse.id/media/catalog/product/cache/e81e4f913a1cad058ef66fea8e95c839/0/2/02-CONVERSE-FFSSBCONA-CONA03576C-Yellow.jpg",
+    },
+    {
+      id: 2,
+      productName: "Converse Run Star Motion Canvas",
+      productPrice: "Rp. 1.799.000",
+      urlImage:
+        "https://www.converse.id/media/catalog/product/cache/144a401d4eb9f312744987a3cea154c3/C/O/CON171546C-1.jpg",
+    },
+    {
+      id: 3,
+      productName: "Converse CTAS Construct Colorblock",
+      productPrice: "Rp. 1.599.000",
+      urlImage:
+        "https://www.converse.id/media/catalog/product/cache/144a401d4eb9f312744987a3cea154c3/0/1/01-CONVERSE-FFSSBCONA-CONA03472C-Grey.jpg",
+    },
+    {
+      id: 4,
+      productName: "Converse Chuck Taylor All Star",
+      productPrice: "Rp. 899.000",
+      urlImage:
+        "https://www.converse.id/media/catalog/product/cache/144a401d4eb9f312744987a3cea154c3/0/1/01-CONVERSE-FFSSBCON0-CON135251C-Black.jpg",
+    },
+    {
+      id: 5,
+      productName: "Converse Unisex Chuck 70 Plus",
+      productPrice: "Rp. 1.599.000",
+      urlImage:
+        "https://www.converse.id/media/catalog/product/cache/144a401d4eb9f312744987a3cea154c3/0/1/01-CONVERSE-FFSSBCONA-CONA02871C-Brown.jpg",
+    },
+  ];
 
+  const product = listProduct.map((product) => (
+    <div className="kotak">
+      <img
+        key={product.id}
+        src={product.urlImage}
+        alt="gambar"
+        className="gambar-produk"
+      />
+      <p key={product.id} className="nama-produk">
+        {product.productName}
+      </p>
+      <p key={product.id} className="harga-produk">
+        {product.productPrice}
+      </p>
+    </div>
+  ));
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <div className="bungkus">{product}</div>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
